fd17250af2e19d51bbbaf03a72d1a25b14b3f7e7
Nadia <nadiyaklokova@gmail.com>
2021-11-23 20:41:39 +0100

    nube y bosque

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-let montaña = document.getElementById('montaña');
-let montaña2 = document.getElementById('montaña2');
+let nube = document.getElementById('nube');
+let nube2 = document.getElementById('nube2');
+let nube3 = document.getElementById('nube3');
 let sol = document.getElementById('sol');
-let bosque = document.getElementById('bosque');
-let castillo = document.getElementById('castillo');
-let texto = document.getElementById('texto');
+let montaña1 = document.getElementById('montaña1');
+let montaña11 = document.getElementById('montaña11');
+let montaña2 = document.getElementById('montaña2');
+let isla = document.getElementById('isla');
+let agua = document.getElementById('agua');
 
 
 window.addEventListener('scroll', function(){
     var value = window.scrollY;
 
-    montaña.style.top = value * 0.5 + 'px';
-    montaña2.style.top = value * 0.15 + 'px';
+    nube.style.top = value * 0.7 + 'px';
+    nube2.style.top = value * 0.6 + 'px';
+    nube3.style.top = value * 0.5 + 'px';
     sol.style.left = -value * 0.5 + 'px'; /* sol.style.left = value * 0.5 + 'px'; */
-    bosque.style.top = -value * 0.02 + 'px';
-    castillo.style.top = value * 0.20 + 'px';
-    texto.style.top = value * 1 + 'px';
+    montaña1.style.top = value * 0.6 + 'px';
+    montaña11.style.top = value * 0.6 + 'px';
+    montaña2.style.top = value * 0.7 + 'px';
+    isla.style.top = value * 0.65 + 'px';
+   agua.style.top = value * 0.68+ 'px';
 })
