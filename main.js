b316e18dfe9a6646d136c72f889aa6023aed3c2a
Nadia <nadiyaklokova@gmail.com>
2021-11-25 09:32:45 +0100

    responseve corregido

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* ===========Texto inicio========= */
+
 let glowInTexts = document.querySelectorAll(".glowIn");
 glowInTexts.forEach(glowInText => {
   let letters = glowInText.textContent.split("");
@@ -9,3 +11,32 @@ glowInTexts.forEach(glowInText => {
     glowInText.append(span);
   });
 });
+
+/* =================MENU=============== */
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('#menu');
+
+function toggleMenu(){
+   if(menu.classList.contains('active')){  /* si el menu contiene una clase activada(osea algo activado) */
+    closeNav() // ejecutar la funcion closeNav(),  mirar mas abajo
+} else{
+    menu.classList.add('active');/* no esta activado, pues activalo */
+    }
+}
+
+toggle.addEventListener('click', toggleMenu, false);
+
+
+function closeNav(){
+    menu.classList.remove('active'); /* entonces que quite este algo activado(lo cierra) */
+}
+
+
+
+/*-------Form------------ */
+const form = document.querySelector('.container-form');
+const btn = document.querySelector('.btn-inicio');
+
+btn.addEventListener('click',() =>{
+  form.style.opacity = 1;
+  })
